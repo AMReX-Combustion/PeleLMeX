7f8c5a60cc637c3f7cf4e4657255f404d161eda1
Lucas Esclapez <lucas.esclapez@gmail.com>
2021-02-22 13:38:56 -0800

    Propagate m_incompressible option throughout the code. Add Setup functions handling derived and tagging.

--- Source/PeleLMSetup.cpp
+++ Source/PeleLMSetup.cpp
@@ -1,9 +1,12 @@
 #include <PeleLM.H>
 #include <AMReX_ParmParse.H>
+#include <PeleLMDeriveFunc.H>
 #include <EOS.H>
 #include <Transport.H>
 
 using namespace amrex;
+
+static Box the_same_box (const Box& b)    { return b;                 }
 
 void PeleLM::Setup() {
    BL_PROFILE_VAR("PeleLM::Setup()", Setup);
@@ -14,17 +17,25 @@ void PeleLM::Setup() {
    // Setup the state variables
    variablesSetup();
 
+   // Derived variables
+   derivedSetup();
+
+   // Tagging setup
+   taggingSetup();
+
    // Initialize Level Hierarchy data
    resizeArray();
 
    // Initiliaze BCs
    setBoundaryConditions();
- 
+
    // Initialize EOS and others
-   amrex::Print() << " Initialization of EOS ... \n";
-   EOS::init();
-   amrex::Print() << " Initialization of Transport ... \n";
-   transport_init();
+   if (!m_incompressible) {
+      amrex::Print() << " Initialization of EOS ... \n";
+      EOS::init();
+      amrex::Print() << " Initialization of Transport ... \n";
+      transport_init();
+   }
 
    // Problem parameters
    prob_parm.reset(new ProbParm{});
@@ -102,6 +113,13 @@ void PeleLM::readParameters() {
       m_phys_bc.setLo(i,lo_bc[i]);
       m_phys_bc.setHi(i,hi_bc[i]);
    }
+
+   // -----------------------------------------
+   // Algorithm
+   // -----------------------------------------
+   pp.query("use_divu", m_has_divu);
+   pp.query("incompressible", m_incompressible);
+   if (m_incompressible) m_has_divu = 0;
 }
 
 void PeleLM::readIOParameters() {
@@ -123,31 +141,147 @@ void PeleLM::variablesSetup() {
    // Variables ordering is defined through macro in PeleLM_Index.H 
    // Simply print on screen the state layout
    Print() << PrettyLine;
-   Print() << " State components \n"; 
+   Print() << " State components \n";
    Print() << PrettyLine;
 
    Print() << " Velocity X: " << VELX;
-#if AMREX_SPACEDIM > 1 
+   stateComponents.emplace_back(VELX,"x_velocity");
+#if AMREX_SPACEDIM > 1
    Print() << ", Velocity Y: " << VELY;
-#if AMREX_SPACEDIM > 2 
+   stateComponents.emplace_back(VELY,"y_velocity");
+#if AMREX_SPACEDIM > 2
    Print() << ", Velocity Z: " << VELZ;
+   stateComponents.emplace_back(VELZ,"z_velocity");
 #endif
 #endif
    Print() << " \n";
 
-   Print() << " Density: " << DENSITY << "\n";
-   Print() << " First species: " << FIRSTSPEC << "\n";
-   Print() << " Enthalpy: " << RHOH << "\n";
-   Print() << " Temperature: " << TEMP << "\n";
-   Print() << " thermo. pressure: " << RHORT << "\n";
+   if (! m_incompressible) {
+      Print() << " Density: " << DENSITY << "\n";
+      stateComponents.emplace_back(DENSITY,"density");
+      Print() << " First species: " << FIRSTSPEC << "\n";
+      Vector<std::string> names;
+      EOS::speciesNames(names);
+      for (int n = 0; n < NUM_SPECIES; n++ ) {
+         stateComponents.emplace_back(FIRSTSPEC+n,"rho.Y("+names[n]+")");
+      }
+      Print() << " Enthalpy: " << RHOH << "\n";
+      stateComponents.emplace_back(RHOH,"rhoh");
+      Print() << " Temperature: " << TEMP << "\n";
+      stateComponents.emplace_back(TEMP,"temp");
+      Print() << " thermo. pressure: " << RHORT << "\n";
+      stateComponents.emplace_back(RHORT,"RhoRT");
+   }
+
    if (m_nAux > 0) {
       Print() << " First passive scalar: " << FIRSTAUX << "\n";
+      for (int n = 0; n < m_nAux; n++ ) {
+         stateComponents.emplace_back(FIRSTAUX+n,"Aux_"+std::to_string(n));
+      }
    }
 
    Print() << " => Total number of state variables: " << NVAR << "\n";
    Print() << PrettyLine;
    Print() << "\n";
 
+}
+
+void PeleLM::derivedSetup()
+{
+   BL_PROFILE_VAR("PeleLM::derivedSetup()", derivedSetup);
+
+   if (!m_incompressible) {
+
+      // Get species names
+      Vector<std::string> spec_names;
+      EOS::speciesNames(spec_names);
+
+      // Set species mass fractions
+      Vector<std::string> var_names_massfrac(NUM_SPECIES);
+      for (int n = 0 ; n < NUM_SPECIES; n++) {
+         var_names_massfrac[n] = "Y("+spec_names[n]+")";
+      }
+      derive_lst.add("mass_fractions",IndexType::TheCellType(),NUM_SPECIES,
+                     var_names_massfrac,pelelm_dermassfrac,the_same_box);
+
+   }
+}
+
+void PeleLM::taggingSetup()
+{
+   BL_PROFILE_VAR("PeleLM::taggingSetup()", taggingSetup);
+
+   std::string amr_prefix = "amr";
+   ParmParse ppamr(amr_prefix);
+
+   Vector<std::string> refinement_indicators;
+   ppamr.queryarr("refinement_indicators",refinement_indicators,0,ppamr.countval("refinement_indicators"));
+   for (int n = 0; n<refinement_indicators.size(); ++n) {
+      std::string ref_prefix = amr_prefix + "." + refinement_indicators[n];
+      ParmParse ppr(ref_prefix);
+
+      // Tag a given box
+      RealBox realbox;
+      if (ppr.countval("in_box_lo")) {
+         Vector<Real> box_lo(AMREX_SPACEDIM);
+         Vector<Real> box_hi(AMREX_SPACEDIM);
+         ppr.getarr("in_box_lo",box_lo,0,box_lo.size());
+         ppr.getarr("in_box_hi",box_hi,0,box_hi.size());
+         realbox = RealBox(&(box_lo[0]),&(box_hi[0]));
+      }
+
+      AMRErrorTagInfo info;
+
+      if (realbox.ok()) {
+         info.SetRealBox(realbox);
+      }
+
+      if (ppr.countval("start_time") > 0) {
+         Real min_time; ppr.get("start_time",min_time);
+         info.SetMinTime(min_time);
+      }
+
+      if (ppr.countval("end_time") > 0) {
+         Real max_time; ppr.get("end_time",max_time);
+         info.SetMaxTime(max_time);
+      }
+
+      if (ppr.countval("max_level") > 0) {
+         int max_level; ppr.get("max_level",max_level);
+         info.SetMaxLevel(max_level);
+      }
+
+      bool itexists = false;
+      if (ppr.countval("value_greater")) {
+         Real value; ppr.get("value_greater",value);
+         std::string field; ppr.get("field_name",field);
+         errTags.push_back(AMRErrorTag(value,AMRErrorTag::GREATER,field,info));
+         itexists = derive_lst.canDerive(field) || isStateVariable(field);
+      } else if (ppr.countval("value_less")) {
+         Real value; ppr.get("value_less",value);
+         std::string field; ppr.get("field_name",field);
+         errTags.push_back(AMRErrorTag(value,AMRErrorTag::LESS,field,info));
+         itexists = derive_lst.canDerive(field) || isStateVariable(field);
+      } else if (ppr.countval("vorticity_greater")) {
+         Real value; ppr.get("vorticity_greater",value);
+         const std::string field="mag_vort";
+         errTags.push_back(AMRErrorTag(value,AMRErrorTag::VORT,field,info));
+         itexists = derive_lst.canDerive(field) || isStateVariable(field);
+      } else if (ppr.countval("adjacent_difference_greater")) {
+         Real value; ppr.get("adjacent_difference_greater",value);
+         std::string field; ppr.get("field_name",field);
+         errTags.push_back(AMRErrorTag(value,AMRErrorTag::GRAD,field,info));
+         itexists = derive_lst.canDerive(field) || isStateVariable(field);
+      } else if (realbox.ok()) {
+        errTags.push_back(AMRErrorTag(info));
+      } else {
+        Abort(std::string("Unrecognized refinement indicator for " + refinement_indicators[n]).c_str());
+      }
+
+      if ( !itexists ) {
+         amrex::Error("PeleLM::taggingSetup(): unknown variable field for criteria "+refinement_indicators[n]);
+      }
+   }
 }
 
 void PeleLM::resizeArray() {
