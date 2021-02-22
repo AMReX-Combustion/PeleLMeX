85f896cde05e0c51763796e20d9e69c6ad24aa20
Lucas Esclapez <lucas.esclapez@gmail.com>
2021-02-22 13:33:42 -0800

    Setup error tagging function. Relies on derive functionality.

--- Source/PeleLMTagging.cpp
+++ Source/PeleLMTagging.cpp
@@ -1,9 +1,28 @@
 #include <PeleLM.H>
+#include <AMReX_TagBox.H>
+
+using namespace amrex;
 
 void
 PeleLM::ErrorEst( int lev,
-                  amrex::TagBoxArray& tags,   
-                  amrex::Real time,
-                  int ng) {
+                  TagBoxArray& tags,
+                  Real time,
+                  int ng)
+{
    BL_PROFILE_VAR("PeleLM::ErrorEst()", ErrorEst);
+
+#ifdef AMREX_USE_EB
+   if (m_refine_cutcells) {
+      const MultiFab& rho = (getLevelDataPtr(lev,AmrNewTime))->density;
+      TagCutCells(tags, rho);
+   }
+#endif
+
+   for (int n = 0; n < errTags.size(); ++n) {
+      std::unique_ptr<MultiFab> mf;
+      if (errTags[n].Field() != std::string()) {
+         mf = derive(errTags[n].Field(), time, lev, errTags[n].NGrow());
+      }
+      errTags[n](tags,mf.get(),TagBox::CLEAR,TagBox::SET,time,lev,geom[lev]);
+   }
 }
