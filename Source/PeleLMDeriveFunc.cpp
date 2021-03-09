fe089ff7626baeb16bd87f7b2baa3cede9073b9f
Lucas Esclapez <lucas.esclapez@gmail.com>
2021-03-08 21:35:05 -0800

    Update signature of the derivve functions to accomodate nodal pressure FAB.

--- Source/PeleLMDeriveFunc.cpp
+++ Source/PeleLMDeriveFunc.cpp
@@ -9,7 +9,8 @@ using namespace amrex;
 // Extract temp
 //
 void pelelm_dertemp (const Box& bx, FArrayBox& derfab, int dcomp, int ncomp,
-                     const FArrayBox& statefab, const Geometry& /*geomdata*/,
+                     const FArrayBox& statefab, const FArrayBox& /*pressfab*/,
+                     const Geometry& /*geomdata*/,
                      Real /*time*/, const Vector<BCRec>& /*bcrec*/, int /*level*/)
 
 {
@@ -29,7 +30,8 @@ void pelelm_dertemp (const Box& bx, FArrayBox& derfab, int dcomp, int ncomp,
 // Extract species mass fractions Y_n
 //
 void pelelm_dermassfrac (const Box& bx, FArrayBox& derfab, int dcomp, int ncomp,
-                         const FArrayBox& statefab, const Geometry& /*geomdata*/,
+                         const FArrayBox& statefab, const FArrayBox& /*pressfab*/,
+                         const Geometry& /*geomdata*/,
                          Real /*time*/, const Vector<BCRec>& /*bcrec*/, int /*level*/)
 
 {
@@ -45,5 +47,33 @@ void pelelm_dermassfrac (const Box& bx, FArrayBox& derfab, int dcomp, int ncomp,
     {   
         amrex::Real rhoinv = 1.0 / in_dat(i,j,k,DENSITY);
         der(i,j,k,n) = in_dat(i,j,k,FIRSTSPEC+n) * rhoinv;
+    }); 
+}
+
+//
+// Compute cell averaged pressure from nodes
+//
+void pelelm_deravgpress (const Box& bx, FArrayBox& derfab, int dcomp, int ncomp,
+                         const FArrayBox& /*statefab*/, const FArrayBox& pressfab,
+                         const Geometry& /*geomdata*/,
+                         Real /*time*/, const Vector<BCRec>& /*bcrec*/, int /*level*/)
+
+{
+    AMREX_ASSERT(derfab.box().contains(bx));
+    auto const in_dat = pressfab.array();
+    auto       der = derfab.array(dcomp);
+    Real factor = 1.0 / ( AMREX_D_TERM(2.0,*2.0,*2.0) );
+    amrex::ParallelFor(bx,
+    [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+    {   
+        der(i,j,k) =  factor * (  in_dat(i+1,j,k)     + in_dat(i,j,k)
+#if (AMREX_SPACEDIM >= 2 )
+                                + in_dat(i+1,j+1,k)   + in_dat(i,j+1,k)
+#if (AMREX_SPACEDIM == 3 )
+                                + in_dat(i+1,j,k+1)   + in_dat(i,j,k+1)
+                                + in_dat(i+1,j+1,k+1) + in_dat(i,j+1,k+1)
+#endif
+#endif
+                                );  
     }); 
 }
