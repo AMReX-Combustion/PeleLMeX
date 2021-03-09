a479cf03d834f27789fd7b85e788a3961fe07a68
Lucas Esclapez <lucas.esclapez@gmail.com>
2021-03-08 21:29:23 -0800

    Update Plot to accomodate derive. Will need to handle the derive nComp.

--- Source/PeleLMPlot.cpp
+++ Source/PeleLMPlot.cpp
@@ -9,11 +9,18 @@ void PeleLM::WritePlotFile() {
 
    const std::string& plotfilename = amrex::Concatenate(m_plot_file, m_nstep);
 
+   if (m_verbose) {
+      amrex::Print() << " Dumping plotfile: " << plotfilename << "\n";
+   }
+
    int ncomp = 0;
    if (m_incompressible) {
-      ncomp = AMREX_SPACEDIM;
+      ncomp = 2*AMREX_SPACEDIM + m_derivePlotVarCount;
    } else {
-      ncomp = NVAR;
+      ncomp = NVAR + AMREX_SPACEDIM + m_derivePlotVarCount;
+      if (m_has_divu) {
+         ncomp += 1;
+      }
    }
 
    // Plot MF  
@@ -26,11 +33,11 @@ void PeleLM::WritePlotFile() {
    EOS::speciesNames(names);
 
    Vector<std::string> plt_VarsName;
-   plt_VarsName.push_back("U");
+   plt_VarsName.push_back("x_velocity");
 #if ( AMREX_SPACEDIM > 1 )
-   plt_VarsName.push_back("V");
+   plt_VarsName.push_back("y_velocity");
 #if ( AMREX_SPACEDIM > 2 )
-   plt_VarsName.push_back("W");
+   plt_VarsName.push_back("z_velocity");
 #endif
 #endif
    if (!m_incompressible) {
@@ -38,19 +45,51 @@ void PeleLM::WritePlotFile() {
       for (int n = 0; n < NUM_SPECIES; n++) {
          plt_VarsName.push_back("rho.Y("+names[n]+")");
       }
-      plt_VarsName.push_back("rhoH");
+      plt_VarsName.push_back("rhoh");
       plt_VarsName.push_back("temp");
-      plt_VarsName.push_back("rhoRT");
+      plt_VarsName.push_back("RhoRT");
+      if (m_has_divu) {
+         plt_VarsName.push_back("divu");
+      }
    }
+
+   plt_VarsName.push_back("gradp_x");
+#if ( AMREX_SPACEDIM > 1 )
+   plt_VarsName.push_back("gradp_y");
+#if ( AMREX_SPACEDIM > 2 )
+   plt_VarsName.push_back("gradp_z");
+#endif
+#endif
    
+   int cnt = 0;
    for (int lev = 0; lev <= finest_level; ++lev) {
-      MultiFab::Copy(mf_plt[lev], m_leveldata_new[lev]->velocity, 0, VELX, AMREX_SPACEDIM, 0);
+      MultiFab::Copy(mf_plt[lev], m_leveldata_new[lev]->velocity, 0, cnt, AMREX_SPACEDIM, 0);
+      cnt += AMREX_SPACEDIM;
       if (!m_incompressible) {
-         MultiFab::Copy(mf_plt[lev], m_leveldata_new[lev]->density, 0, DENSITY, 1, 0);
-         MultiFab::Copy(mf_plt[lev], m_leveldata_new[lev]->species, 0, FIRSTSPEC, NUM_SPECIES, 0);
-         MultiFab::Copy(mf_plt[lev], m_leveldata_new[lev]->rhoh, 0, RHOH, 1, 0);
-         MultiFab::Copy(mf_plt[lev], m_leveldata_new[lev]->temp, 0, TEMP, 1, 0);
-         MultiFab::Copy(mf_plt[lev], m_leveldata_new[lev]->rhoRT, 0, RHORT, 1, 0);
+         MultiFab::Copy(mf_plt[lev], m_leveldata_new[lev]->density, 0, cnt, 1, 0);
+         cnt += 1;
+         MultiFab::Copy(mf_plt[lev], m_leveldata_new[lev]->species, 0, cnt, NUM_SPECIES, 0);
+         cnt += NUM_SPECIES;
+         MultiFab::Copy(mf_plt[lev], m_leveldata_new[lev]->rhoh, 0, cnt, 1, 0);
+         cnt += 1;
+         MultiFab::Copy(mf_plt[lev], m_leveldata_new[lev]->temp, 0, cnt, 1, 0);
+         cnt += 1;
+         MultiFab::Copy(mf_plt[lev], m_leveldata_new[lev]->rhoRT, 0, cnt, 1, 0);
+         cnt += 1;
+         if (m_has_divu) {
+            MultiFab::Copy(mf_plt[lev], m_leveldata_new[lev]->divu, 0, cnt, 1, 0);
+            cnt += 1;
+         }
+      }
+      MultiFab::Copy(mf_plt[lev], m_leveldata_new[lev]->gp, 0, cnt,AMREX_SPACEDIM,0);
+      cnt += AMREX_SPACEDIM;
+
+      for (int ivar = 0; ivar < m_derivePlotVarCount; ivar++ ) {
+         plt_VarsName.push_back(m_derivePlotVars[ivar]);
+         std::unique_ptr<MultiFab> mf;
+         mf = derive(m_derivePlotVars[ivar], m_cur_time, lev, 0);
+         MultiFab::Copy(mf_plt[lev], *mf, 0, cnt, 1, 0);
+         cnt += 1;
       }
    }
 
