105f6c7e3034793bc982328e675a17e2d45ecdf3
Lucas Esclapez <lesclapez@lbl.gov>
2021-03-30 23:26:41 -0700

    Create a first Efield flamesheet case.

---
+++ Exec/RegTests/Efield/pelelm_prob.cpp
@@ -0,0 +1,18 @@
+#include <PeleLM.H>
+#include <AMReX_ParmParse.H>
+#include <pmf.H>
+
+void PeleLM::readProbParm()
+{
+   amrex::ParmParse pp("prob");
+   
+   std::string type;
+   pp.query("P_mean", prob_parm->P_mean);
+   pp.query("standoff", PeleLM::prob_parm->standoff);
+   pp.query("pertmag",  PeleLM::prob_parm->pertmag);
+
+   std::string pmf_datafile;
+   pp.query("pmf_datafile", pmf_datafile);
+   int pmf_do_average = 1;
+   PMF::read_pmf(pmf_datafile, pmf_do_average);
+}
