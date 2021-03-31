7b0e73cb9509889928472546c6280309ba7a3bad
Lucas Esclapez <lesclapez@lbl.gov>
2021-03-31 15:39:17 -0700

    Setup PhiV dirichlet BC values in efield case.

--- Exec/RegTests/Efield/pelelm_prob.cpp
+++ Exec/RegTests/Efield/pelelm_prob.cpp
@@ -11,6 +11,9 @@ void PeleLM::readProbParm()
    pp.query("standoff", PeleLM::prob_parm->standoff);
    pp.query("pertmag",  PeleLM::prob_parm->pertmag);
 
+   pp.query("PhiV_y_hi", PeleLM::prob_parm->phiV_hiy);
+   pp.query("PhiV_y_lo", PeleLM::prob_parm->phiV_loy);
+
    std::string pmf_datafile;
    pp.query("pmf_datafile", pmf_datafile);
    int pmf_do_average = 1;
