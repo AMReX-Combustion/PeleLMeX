5d11957a818732a4905f2b30fe10066f795f8a1b
Lucas Esclapez <lesclapez@lbl.gov>
2021-04-09 09:23:14 -0700

    Accessor to nlState vector.

--- Source/Efield/PeleLMEFUtils.cpp
+++ Source/Efield/PeleLMEFUtils.cpp
@@ -14,12 +14,32 @@ PeleLM::getNLgradPhiVVect() {
    return r;
 }
 
+Vector<Array<MultiFab*,AMREX_SPACEDIM>>
+PeleLM::getUeffVect() {
+   Vector<Array<MultiFab*,AMREX_SPACEDIM>> r;
+   r.reserve(finest_level+1);
+   for (int lev = 0; lev <= finest_level; ++lev) {
+      r.push_back(GetArrOfPtrs(m_leveldatanlsolve[lev]->uEffnE));
+   }
+   return r;
+}
+
 Vector<MultiFab*>
 PeleLM::getNLresidVect() {
    Vector<MultiFab*> r;
    r.reserve(finest_level+1);
    for (int lev = 0; lev <= finest_level; ++lev) {
       r.push_back(&(m_leveldatanlsolve[lev]->nlResid));
+   }
+   return r;
+}
+
+Vector<MultiFab*>
+PeleLM::getNLstateVect() {
+   Vector<MultiFab*> r;
+   r.reserve(finest_level+1);
+   for (int lev = 0; lev <= finest_level; ++lev) {
+      r.push_back(&(m_leveldatanlsolve[lev]->nlState));
    }
    return r;
 }
