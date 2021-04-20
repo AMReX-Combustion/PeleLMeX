2314265bd7cb7f2dd71d9ed27a477b47a44817fa
Lucas Esclapez <lesclapez@lbl.gov>
2021-04-20 13:45:43 -0700

    Add EFUtils to get/apply scaling of the NL solve Vectors.

--- Source/Efield/PeleLMEFUtils.cpp
+++ Source/Efield/PeleLMEFUtils.cpp
@@ -43,3 +43,45 @@ PeleLM::getNLstateVect() {
    }
    return r;
 }
+
+void PeleLM::getNLStateScaling(Real &nEScale, Real &phiVScale)
+{
+   Array<Real,2> r = {0.0,0.0};
+   for (int comp = 0; comp < 2; comp++) {
+      for (int lev = 0; lev <= finest_level; ++lev) {
+         r[comp] = std::max(r[comp], m_leveldatanlsolve[lev]->nlState.norm0(comp,0,true,true));
+      }
+      ParallelDescriptor::ReduceRealMax(r[comp]);
+   }
+   nEScale = r[0];
+   phiVScale = r[1];
+}
+
+void PeleLM::getNLResidScaling(Real &nEScale, Real &phiVScale)
+{
+   Array<Real,2> r = {0.0,0.0};
+   for (int comp = 0; comp < 2; comp++) {
+      for (int lev = 0; lev <= finest_level; ++lev) {
+         r[comp] = std::max(r[comp], m_leveldatanlsolve[lev]->nlResid.norm0(comp,0,true,true));
+      }
+      ParallelDescriptor::ReduceRealMax(r[comp]);
+   }
+   nEScale = r[0];
+   phiVScale = r[1];
+}
+
+void PeleLM::scaleNLState(const Real &nEScale, const Real &phiVScale)
+{
+   for (int lev = 0; lev <= finest_level; ++lev) {
+      m_leveldatanlsolve[lev]->nlState.mult(1.0/nE_scale,0,1,1);
+      m_leveldatanlsolve[lev]->nlState.mult(1.0/phiV_scale,1,1,1);
+   }
+}
+
+void PeleLM::scaleNLResid(const Vector<MultiFab*> &a_resid, const Real &nEScale, const Real &phiVScale)
+{
+   for (int lev = 0; lev <= finest_level; ++lev) {
+      a_resid[lev]->mult(1.0/FnE_scale,0,1,1);
+      a_resid[lev]->mult(1.0/FphiV_scale,1,1,1);
+   }
+}
