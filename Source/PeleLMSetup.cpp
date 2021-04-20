e41666e092c0a7c5f237cdd38a89ce8764656883
Lucas Esclapez <lesclapez@lbl.gov>
2021-04-20 13:48:27 -0700

    Get some of the Efield ParmParse attributes. Might move that elsewhere.

--- Source/PeleLMSetup.cpp
+++ Source/PeleLMSetup.cpp
@@ -256,6 +256,18 @@ void PeleLM::readParameters() {
       ppa.query("regrid_int", m_regrid_int);
    }
 
+#ifdef PLM_USE_EFIELD
+   // -----------------------------------------
+   // EFIELD
+   // -----------------------------------------
+   ppef.query("JFNK_newtonTol",m_ef_newtonTol);
+   ppef.query("JFNK_maxNewton",m_ef_maxNewtonIter);
+   ppef.query("GMRES_verbose",m_ef_GMRES_verbose);
+   ppef.query("GMRES_max_restart",m_ef_GMRES_maxRst);
+   ppef.query("GMRES_rel_tol",m_ef_GMRES_reltol);
+
+#endif
+
 }
 
 void PeleLM::readIOParameters() {
