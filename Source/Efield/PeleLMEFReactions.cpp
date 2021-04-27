8a3c79666b2e6ed96c6de4ea3adfb76c04255c82
Lucas Esclapez <lucas.esclapez@gmail.com>
2021-04-27 16:22:21 -0700

    Add EF reaction/timestep constrain functions.

---
+++ Source/Efield/PeleLMEFReactions.cpp
@@ -0,0 +1,34 @@
+#include <PeleLM.H>
+#include <PeleLMEF_K.H>
+
+using namespace amrex;
+
+void PeleLM::computeInstantaneousReactionRateEF(int lev,
+                                                const TimeStamp &a_time,
+                                                const MultiFab &a_mask,
+                                                MultiFab* a_I_R)
+{
+   auto ldata_p = getLevelDataPtr(lev,a_time);
+
+#ifdef _OPENMP
+#pragma omp parallel if (Gpu::notInLaunchRegion())
+#endif
+   for (MFIter mfi(ldata_p->species,TilingIfNotGPU()); mfi.isValid(); ++mfi)
+   {
+      const Box& bx = mfi.tilebox();
+      auto const& rhoY    = ldata_p->species.const_array(mfi);
+      auto const& rhoH    = ldata_p->rhoh.const_array(mfi);
+      auto const& nE      = ldata_p->nE.const_array(mfi);
+      auto const& T       = ldata_p->temp.const_array(mfi);
+      auto const& mask    = a_mask.const_array(mfi);
+      auto const& rhoYdot = a_I_R->array(mfi);
+      auto const& nEdot   = a_I_R->array(mfi,NUM_SPECIES);
+
+      amrex::ParallelFor(bx, [rhoY, rhoH, nE, T, mask, rhoYdot, nEdot]
+      AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+      {
+         reactionRateRhoY_EF( i, j, k, rhoY, rhoH, T, nE, mask,
+                             rhoYdot, nEdot );
+      });
+   }
+}
