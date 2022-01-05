c9aefc35e356838cf2808203d3730a7ca24e67e3
Lucas Esclapez <13371051+esclapez@users.noreply.github.com>
2022-01-04 19:38:47 -0800

    Adding sumodules to LMeX (#41)

--- Exec/RegTests/EB_EnclosedFlame/pelelm_prob.cpp
+++ Exec/RegTests/EB_EnclosedFlame/pelelm_prob.cpp
@@ -9,14 +9,9 @@ void PeleLM::readProbParm()
    pp.query("P_mean",   PeleLM::prob_parm->P_mean);
    pp.query("standoff", PeleLM::prob_parm->standoff);
    pp.query("pertmag",  PeleLM::prob_parm->pertmag);
+#if (AMREX_SPACEDIM == 3)
+   pp.query("pertLz",  PeleLM::prob_parm->pertLz);
+#endif
 
    PeleLM::pmf_data.initialize();
-
-   auto& trans_parm = PeleLM::trans_parms.host_trans_parm();
-   amrex::ParmParse pptr("transport");
-   pp.query("const_viscosity", trans_parm.const_viscosity);
-   pp.query("const_bulk_viscosity", trans_parm.const_bulk_viscosity);
-   pp.query("const_conductivity", trans_parm.const_conductivity);
-   pp.query("const_diffusivity", trans_parm.const_diffusivity);
-   PeleLM::trans_parms.sync_to_device();
 }
