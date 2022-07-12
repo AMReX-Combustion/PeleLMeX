f11cd213d9dd949cb4912fbcdb6e18636e356b9e
Lucas Esclapez <lesclapez@lbl.gov>
2022-07-11 22:54:03 -0700

    Split diffusion solve between neutral species and individual solve for each ions (because BC type might change).

--- Source/PeleLMDiffusion.cpp
+++ Source/PeleLMDiffusion.cpp
@@ -169,6 +169,25 @@ void PeleLM::computeDifferentialDiffusionFluxes(const TimeStamp &a_time,
    // Get the species BCRec
    auto bcRecSpec = fetchBCRecArray(FIRSTSPEC,NUM_SPECIES);
 
+#ifdef PELE_USE_EFIELD
+   // Get the species diffusion fluxes from the DiffusionOp
+   // Don't average down just yet
+   int do_avgDown = 0;
+   getMCDiffusionOp(NUM_SPECIES-NUM_IONS)->computeDiffFluxes(a_fluxes, 0,
+                                                             GetVecOfConstPtrs(getSpeciesVect(a_time)), 0,
+                                                             GetVecOfConstPtrs(getDensityVect(a_time)),
+                                                             GetVecOfConstPtrs(getDiffusivityVect(a_time)), 0, bcRecSpec,
+                                                             NUM_SPECIES-NUM_IONS, -1.0, do_avgDown);
+   // Ions one by one
+   for ( int n = 0; n < NUM_IONS; n++) {
+      auto bcRecIons = fetchBCRecArray(FIRSTSPEC+NUM_SPECIES-NUM_IONS+n,1);
+      getDiffusionOp()->computeDiffFluxes(a_fluxes, NUM_SPECIES-NUM_IONS+n,
+                                          GetVecOfConstPtrs(getSpeciesVect(a_time)), NUM_SPECIES-NUM_IONS+n,
+                                          GetVecOfConstPtrs(getDensityVect(a_time)),
+                                          GetVecOfConstPtrs(getDiffusivityVect(a_time)), 0, bcRecIons,
+                                          1, -1.0, do_avgDown);
+   }
+#else
    // Get the species diffusion fluxes from the DiffusionOp
    // Don't average down just yet
    int do_avgDown = 0;
@@ -177,6 +196,7 @@ void PeleLM::computeDifferentialDiffusionFluxes(const TimeStamp &a_time,
                                                     GetVecOfConstPtrs(getDensityVect(a_time)),
                                                     GetVecOfConstPtrs(getDiffusivityVect(a_time)), 0, bcRecSpec,
                                                     NUM_SPECIES, -1.0, do_avgDown);
+#endif
 
    // Add the wbar term
    if (m_use_wbar) {
@@ -608,6 +628,29 @@ void PeleLM::differentialDiffusionUpdate(std::unique_ptr<AdvanceAdvData> &advDat
    // Get the species BCRec
    auto bcRecSpec = fetchBCRecArray(FIRSTSPEC,NUM_SPECIES);
 
+#ifdef PELE_USE_EFIELD
+   // Solve for \widetilda{rhoY^{np1,kp1}}
+   // -> return the uncorrected fluxes^{np1,kp1}
+   // -> and the partially updated species (not including wbar or flux correction)
+   getMCDiffusionOp(NUM_SPECIES-NUM_IONS)->diffuse_scalar(GetVecOfPtrs(getSpeciesVect(AmrNewTime)), 0,
+                                                          GetVecOfConstPtrs(advData->Forcing), 0,
+                                                          GetVecOfArrOfPtrs(fluxes), 0,
+                                                          GetVecOfConstPtrs(getDensityVect(AmrNewTime)),        // this is the acoeff of LinOp
+                                                          GetVecOfConstPtrs(getDensityVect(AmrNewTime)),        // this triggers proper scaling by density
+                                                          GetVecOfConstPtrs(getDiffusivityVect(AmrNewTime)), 0, bcRecSpec,
+                                                          NUM_SPECIES-NUM_IONS, 0, m_dt);
+   // Ions one by one
+   for ( int n = 0; n < NUM_IONS; n++) {
+      auto bcRecIons = fetchBCRecArray(FIRSTSPEC+NUM_SPECIES-NUM_IONS+n,1);
+      getDiffusionOp()->diffuse_scalar(GetVecOfPtrs(getSpeciesVect(AmrNewTime)), NUM_SPECIES-NUM_IONS+n,
+                                       GetVecOfConstPtrs(advData->Forcing), NUM_SPECIES-NUM_IONS+n,
+                                       GetVecOfArrOfPtrs(fluxes), NUM_SPECIES-NUM_IONS+n,
+                                       GetVecOfConstPtrs(getDensityVect(AmrNewTime)),        // this is the acoeff of LinOp
+                                       GetVecOfConstPtrs(getDensityVect(AmrNewTime)),        // this triggers proper scaling by density
+                                       GetVecOfConstPtrs(getDiffusivityVect(AmrNewTime)), 0, bcRecIons,
+                                       1, 0, m_dt);
+   }
+#else
    // Solve for \widetilda{rhoY^{np1,kp1}}
    // -> return the uncorrected fluxes^{np1,kp1}
    // -> and the partially updated species (not including wbar or flux correction)
@@ -618,6 +661,7 @@ void PeleLM::differentialDiffusionUpdate(std::unique_ptr<AdvanceAdvData> &advDat
                                                  GetVecOfConstPtrs(getDensityVect(AmrNewTime)),        // this triggers proper scaling by density
                                                  GetVecOfConstPtrs(getDiffusivityVect(AmrNewTime)), 0, bcRecSpec,
                                                  NUM_SPECIES, 0, m_dt);
+#endif
 
    // Add lagged Wbar term
    // Computed in computeDifferentialDiffusionTerms at t^{n} if first SDC iteration, t^{np1,k} otherwise
