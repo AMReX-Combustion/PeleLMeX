0988036ae6ce7b6da8c931bc77d9b55026c939fd
Landon Owen <ldowen@sandia.gov>
2022-11-18 13:03:36 -0800

    Update spray test to use uniform spray initialize function, time was also added as a spray variable

--- Exec/RegTests/SprayTest/pelelm_prob.cpp
+++ Exec/RegTests/SprayTest/pelelm_prob.cpp
@@ -11,14 +11,4 @@ void PeleLM::readProbParm()
    pp.query("init_vel", PeleLM::prob_parm->vel);
    pp.query("init_N2", PeleLM::prob_parm->Y_N2);
    pp.query("init_O2", PeleLM::prob_parm->Y_O2);
-   // Find the number of redistributions during particle initialization
-   pp.query("init_redist", PeleLM::prob_parm->numRedist);
-   pp.query("num_particles", PeleLM::prob_parm->partNum);
-   std::array<amrex::Real, AMREX_SPACEDIM> pvel;
-   pp.query<amrex::Real>("part_vel", pvel);
-   for (int dir = 0; dir < AMREX_SPACEDIM; ++dir) {
-     PeleLM::prob_parm->partVel[dir] = pvel[dir];
-   }
-   pp.get("part_dia", PeleLM::prob_parm->partDia);
-   pp.get("part_temp", PeleLM::prob_parm->partTemp);
 }
