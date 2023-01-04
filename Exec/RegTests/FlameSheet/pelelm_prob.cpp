ee8acc0dd3a943989a1711ed05ac199b48823066
Lucas Esclapez <13371051+esclapez@users.noreply.github.com>
2023-01-04 11:43:22 -0800

    Add fixed perturbation length option in FlameSheet (#166)
    * Finally add control over the perturbation length in FlameSheet.
    
    * Specify pertlength in DRM tests for consistency.

--- Exec/RegTests/FlameSheet/pelelm_prob.cpp
+++ Exec/RegTests/FlameSheet/pelelm_prob.cpp
@@ -9,6 +9,7 @@ void PeleLM::readProbParm()
    pp.query("P_mean",   PeleLM::prob_parm->P_mean);
    pp.query("standoff", PeleLM::prob_parm->standoff);
    pp.query("pertmag",  PeleLM::prob_parm->pertmag);
+   pp.query("pertlength",  PeleLM::prob_parm->pertlength);
 
    PeleLM::pmf_data.initialize();
 }
