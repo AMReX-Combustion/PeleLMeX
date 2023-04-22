5cbf244c3b0c0681505c67b8d6a23f89e9a689df
Lucas Esclapez <13371051+esclapez@users.noreply.github.com>
2023-04-22 10:57:14 -0700

    Tutorial Backward facing step flame (#196)
    * Update/clean input file for Backfacing step tutorial.
    
    * Remove unused code in EB_BFS case.
    
    * Update base input file for Tutorial.
    
    * Initiate a EB Backward facing step flame tutorial.
    
    * Add missing images for tutorial.
    
    * Cont' BFS Tuto.
    
    * Wrap up BFS tutorial.
    
    * fix some typos in BFS tutorial
    
    * Address Bruce's comment.
    
    ---------
    
    Co-authored-by: Bruce Perry <bruce.perry@nrel.gov>

--- Exec/RegTests/EB_BackwardStepFlame/pelelm_prob.cpp
+++ Exec/RegTests/EB_BackwardStepFlame/pelelm_prob.cpp
@@ -7,6 +7,9 @@ void PeleLM::readProbParm()
 
    pp.query("T_mean", prob_parm->T_mean);
    pp.query("P_mean", prob_parm->P_mean);
-   pp.query("meanFlowDir", prob_parm->meanFlowDir);
+   pp.query("Y_fuel", prob_parm->Y_fuel);
+   pp.query("Y_oxid", prob_parm->Y_o2);
+   pp.query("T_hot", prob_parm->T_hot);
+   pp.query("T_wall", prob_parm->Twall);
    pp.query("meanFlowMag", prob_parm->meanFlowMag);
 }
