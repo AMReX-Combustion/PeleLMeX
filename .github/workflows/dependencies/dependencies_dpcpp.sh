42a89648101374a473327ae56065337d5f3be882
Bruce Perry <bruce.perry@nrel.gov>
2023-07-24 12:05:44 -0600

    fix intel CI hopefully

--- .github/workflows/dependencies/dependencies_dpcpp.sh
+++ .github/workflows/dependencies/dependencies_dpcpp.sh
@@ -9,7 +9,7 @@ set -eu -o pipefail
 
 # Ref.: https://github.com/rscohn2/oneapi-ci
 # intel-basekit intel-hpckit are too large in size
-wget -q -O - https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB \
+wget -q -O - https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB \
   | sudo apt-key add -
 echo "deb https://apt.repos.intel.com/oneapi all main" \
   | sudo tee /etc/apt/sources.list.d/oneAPI.list
@@ -18,7 +18,7 @@ sudo apt-get update
 
 sudo apt-get install -y --no-install-recommends \
     build-essential \
-    intel-oneapi-dpcpp-cpp-compiler intel-oneapi-mkl-devel \
+    intel-oneapi-compiler-dpcpp-cpp intel-oneapi-mkl-devel \
     g++             \
     libopenmpi-dev  \
     openmpi-bin
