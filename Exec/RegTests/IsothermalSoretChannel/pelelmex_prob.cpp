322a452cd9319a8dd9ef67aa4cd4638a9b388f89
Thomas Howarth <t.howarth@itv.rwth-aachen.de>
2024-06-24 14:04:50 +0200

    make test case more robust

--- Exec/RegTests/IsothermalSoretChannel/pelelmex_prob.cpp
+++ Exec/RegTests/IsothermalSoretChannel/pelelmex_prob.cpp
@@ -7,10 +7,28 @@ PeleLM::readProbParm()
   amrex::ParmParse pp("prob");
 
   pp.query("P_mean", PeleLM::prob_parm->P_mean);
+  pp.query("Vin", PeleLM::prob_parm->Vin);
+  pp.query("Thigh", PeleLM::prob_parm->Thigh);
+  pp.query("Tlow", PeleLM::prob_parm->Tlow);
 
-  // TODO: somewhat hard coded bath, fuel and oxid IDs
-  // should exist somewhere in PeleLM.
   PeleLM::prob_parm->bathID = N2_ID;
-  PeleLM::prob_parm->fuelID = H2_ID;
   PeleLM::prob_parm->oxidID = O2_ID;
+  // get the fuel name from the peleLM.fuel_name declaration
+  amrex::ParmParse pp_pele("peleLM");
+  std::string fuelName = "";
+  pp_pele.get("fuel_name", fuelName);
+  amrex::Print() << "H2_ID 1 " << H2_ID << std::endl;
+#if defined(H2_ID)
+  if (fuelName == "H2") {
+    PeleLM::prob_parm->fuelID = H2_ID;
+  } else
+#endif
+#if defined(CH4_ID)
+    if (fuelName == "CH4") {
+    PeleLM::prob_parm->fuelID = CH4_ID;
+  } else
+#endif
+  {
+    amrex::Abort("fuel_name not recognised!");
+  }
 }
