fa2ac86ca476fe741163d2ff88670e2b67f2568e
dmontgomeryNREL <David.Montgomery@nrel.gov>
2025-05-27 17:38:17 -0600

    Add Y_drop param for droplet composition

--- Exec/RegTests/DropletEvaporation/SprayParticlesInitInsert.cpp
+++ Exec/RegTests/DropletEvaporation/SprayParticlesInitInsert.cpp
@@ -25,6 +25,8 @@ SprayParticleContainer::InitSprayParticles(const bool init_parts)
   amrex::IntVect partNum(AMREX_D_DECL(100, 100, 100));
   // Find the number of redistributions during particle initialization
   pp.query("init_redist", numRedist);
+
+  // Get particle specs
   pp.query("num_particles", partNum);
   amrex::RealVect partVel;
   std::array<amrex::Real, AMREX_SPACEDIM> pvel = {0.0};
@@ -35,8 +37,22 @@ SprayParticleContainer::InitSprayParticles(const bool init_parts)
   amrex::Real partDia, partTemp;
   pp.get("part_dia", partDia);
   pp.get("part_temp", partTemp);
+
+  // Get composition of liquid
   std::array<amrex::Real, SPRAY_FUEL_NUM> partY = {0.0};
-  partY[0] = 1.;
+  if (SPRAY_FUEL_NUM > 1) {
+    pp.query<amrex::Real>("Y_drop", partY);
+    amrex::Real sumtest = 0.;
+    for (int n = 0; n < SPRAY_FUEL_NUM; ++n) {
+      sumtest += partY[n];
+    }
+    if (std::abs(1. - sumtest) > 1.E-6) {
+      amrex::Abort("Liquid mass fractions must sum to 1!");
+    }
+  } else {
+    partY[0] = 1.;
+  }
+  // Initialize particles using uniform distribution
   uniformSprayInit(
     partNum, partVel, partDia, partTemp, partY.begin(), level, numRedist);
 }
