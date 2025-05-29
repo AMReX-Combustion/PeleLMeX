6aafa729ad2512f4cd6b4fa65f35e363e423a168
dmontgomeryNREL <David.Montgomery@nrel.gov>
2025-05-29 15:19:19 -0600

    Update print statements in prob.cpp

--- Exec/RegTests/SprayEvap/pelelmex_prob.cpp
+++ Exec/RegTests/SprayEvap/pelelmex_prob.cpp
@@ -25,44 +25,52 @@ PeleLM::readProbParm()
   pp.query("part_dia", drop_dia);
   pp.query("part_temp", T_d);
 
+  // Check that velocity is well defined
   if (Re > 0. && std::abs(PeleLM::prob_parm->vel_gas) > 0.) {
     amrex::Abort("Cannot specify droplet velocity and Reynolds number");
   }
-  if (Re > 0.) {
-    amrex::Real massfrac[NUM_SPECIES] = {0.0};
-    massfrac[N2_ID] = PeleLM::prob_parm->Y_N2;
-    massfrac[O2_ID] = PeleLM::prob_parm->Y_O2;
-    amrex::Real T_g = PeleLM::prob_parm->T0_gas;
-    amrex::Real T_eff = (2.*T_d + T_g) / 3.;
-    amrex::Real p_cgs = m2c::P(PeleLM::prob_parm->P_mean);
-    amrex::Real rho_cgs = 0.;
-    eos.PYT2R(p_cgs, massfrac, T_eff, rho_cgs);
 
-    // Calculate transport properties from Simple transport model
-    bool FA = false;
-    bool TR = true;
-    amrex::Real dummy_xi, dummy_chi_mix;
-    amrex::Real mu_cgs, lambda_cgs = 0.; 
-    amrex::Real rhoDi_cgs[NUM_SPECIES], Di[NUM_SPECIES] = {0.0};
-    auto trans = pele::physics::PhysicsType::transport();
-    const auto* trans_parm = &(PeleLM::trans_parms.host_parm());
-    trans.transport
-    (
-      FA, TR, TR, TR, FA, T_eff, 
-      rho_cgs, massfrac, rhoDi_cgs, &dummy_chi_mix, 
-      mu_cgs, dummy_xi, lambda_cgs, trans_parm
-    );
-    
+  // Calculate transport properties from Simple transport model
+  amrex::Real massfrac[NUM_SPECIES] = {0.0};
+  massfrac[N2_ID] = PeleLM::prob_parm->Y_N2;
+  massfrac[O2_ID] = PeleLM::prob_parm->Y_O2;
+  amrex::Real T_g = PeleLM::prob_parm->T0_gas;
+  amrex::Real T_eff = (2.*T_d + T_g) / 3.;
+  amrex::Real p_cgs = m2c::P(PeleLM::prob_parm->P_mean);
+  amrex::Real rho_cgs = 0.;
+  eos.PYT2R(p_cgs, massfrac, T_eff, rho_cgs);
+
+  bool FA = false;
+  bool TR = true;
+  amrex::Real dummy_xi, dummy_chi_mix;
+  amrex::Real mu_cgs, lambda_cgs = 0.;
+  amrex::Real rhoDi_cgs[NUM_SPECIES], Di[NUM_SPECIES] = {0.0};
+  auto trans = pele::physics::PhysicsType::transport();
+  const auto* trans_parm = &(PeleLM::trans_parms.host_parm());
+  trans.transport
+  (
+    FA, TR, TR, TR, FA, T_eff, 
+    rho_cgs, massfrac, rhoDi_cgs, &dummy_chi_mix, 
+    mu_cgs, dummy_xi, lambda_cgs, trans_parm
+  );
+
+  amrex::Real mu = c2m::Mu(mu_cgs);
+  amrex::Real rho = c2m::Rho(rho_cgs);
+
+  // Set umax from Re
+  if (Re > 0.) {
     // Get gas velocity from Re
-    amrex::Real umax = c2m::Mu(mu_cgs) * Re / (c2m::Rho(rho_cgs) * drop_dia);
-    PeleLM::prob_parm->vel_gas = umax;
-    amrex::Print() << "Re = " << Re << "\n"
-                   << "vel_gas = " << prob_parm->vel_gas << "\n"
-                   << "mu_r = " << c2m::Mu(mu_cgs) << "\n"
-                   << "rho_r = "<< c2m::Rho(rho_cgs) << "\n"
-                   << "drop_dia = "<< drop_dia << std::endl;
+    PeleLM::prob_parm->vel_gas = mu * Re / (rho * drop_dia);
+  }
+  else{
+    Re = PeleLM::prob_parm->vel_gas * rho * drop_dia / mu;
   }
 
+  amrex::Print() << "Re = " << Re << "\n"
+                 << "vel_gas = " << prob_parm->vel_gas << "\n"
+                 << "mu_r = " << c2m::Mu(mu_cgs) << "\n"
+                 << "rho_r = "<< c2m::Rho(rho_cgs) << "\n"
+                 << "drop_dia = "<< drop_dia << std::endl;
 }
 
 void
