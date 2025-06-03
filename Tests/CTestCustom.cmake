08a5fa6c4ed5a62a1895a5c618bb7ab4da863438
Jon Rood <jon.rood@nrel.gov>
2025-06-03 10:41:51 -0600

    Ignore warning in SUNDIALS.

--- Tests/CTestCustom.cmake
+++ Tests/CTestCustom.cmake
@@ -1,5 +1,6 @@
 set(CTEST_CUSTOM_WARNING_EXCEPTION ".*: warning: could not create compact unwind*"
                                    ".*has no symbols*"
-                                   ".*submake: resetting jobserver mode*")
+                                   ".*submake: resetting jobserver mode*"
+                                   ".*sunctx_local_scope*")
 set(CTEST_CUSTOM_MAXIMUM_FAILED_TEST_OUTPUT_SIZE "262144")
 set(CTEST_CUSTOM_MAXIMUM_PASSED_TEST_OUTPUT_SIZE "51200")
