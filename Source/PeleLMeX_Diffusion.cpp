80b4c0571bca024a6b7e5644964cc1c170e1025d
Jon Rood <jon.rood@nrel.gov>
2025-08-12 11:56:44 -0600

    Fix clang-tidy warnings.

--- Source/PeleLMeX_Diffusion.cpp
+++ Source/PeleLMeX_Diffusion.cpp
@@ -787,7 +787,7 @@ PeleLM::addWbarTerm(
           // Wbar flux is : - \rho Y_m / W_k * D_m * \nabla
           // \overline{W} with beta_m = \rho * D_m * overline(W) / W_k below
           // need to divide by \overline(W)
-          const auto* const eosparm eosparm = leosparm;
+          const auto* const eosparm = leosparm;
           amrex::ParallelFor(
             ebx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
               auto eos = pele::physics::PhysicsType::eos(eosparm);
