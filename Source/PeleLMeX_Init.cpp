2615dc847b59e48a22cb2629560e02a70cac0362
Jon Rood <jon.rood@nrel.gov>
2025-08-13 19:52:33 -0600

    Fix plasma.

--- Source/PeleLMeX_Init.cpp
+++ Source/PeleLMeX_Init.cpp
@@ -87,7 +87,7 @@ PeleLM::MakeNewLevelFromScratch(
     new LevelDataNLSolve(grids[lev], dmap[lev], *m_factory[lev], m_nGrowState));
   if (m_do_extraEFdiags) {
     m_ionsFluxes[lev].reset(
-      new MultiFab(grids[lev], dmap[lev], NUM_IONS * AMREX_SPACEDIM, 0));
+      new amrex::MultiFab(grids[lev], dmap[lev], NUM_IONS * AMREX_SPACEDIM, 0));
   }
 #endif
 
