caebdac5f031b568479e5b73bd20bd526170ce41
SreejithNREL <Sreejith.NadakkalAppukuttan@nrel.gov>
2025-09-26 15:31:58 -0600

    Trying to debug and refine the newly added spray manifold feature

--- Source/PeleLMeX_Diffusion.cpp
+++ Source/PeleLMeX_Diffusion.cpp
@@ -818,11 +818,6 @@ PeleLM::addWbarTerm(
           // Wbar flux is : - \rho Y_m / W_k * D_m * \nabla
           // \overline{W} with beta_m = \rho * D_m * overline(W) / W_k below
           // need to divide by \overline(W)
-          //<<<<<<< HEAD
-          //          auto* eosparm = leosparm;
-          //          auto eos = pele::physics::PhysicsType::eos(eosparm);
-          //=======
-          //>>>>>>> development
 
           amrex::ParallelFor(
             ebx, [rhoY, gradWbar_ar, beta_ar, spFlux_ar, spwbarFlux_ar,
--- Source/PeleLMeX_Setup.cpp
+++ Source/PeleLMeX_Setup.cpp
@@ -840,9 +840,6 @@ PeleLM::checkSetupParams()
       amrex::Abort("Soot models are not yet supported for Manifold EOS");
     }
 #endif
-#ifdef PELE_USE_SPRAY
-    // amrex::Abort("Spray models are not yet supported for Manifold EOS");
-#endif
 #ifdef PELE_USE_PLASMA
     amrex::Abort("Plasma models are not yet supported for Manifold EOS");
 #endif
