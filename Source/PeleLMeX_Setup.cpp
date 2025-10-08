42683aa6757b862256cd654ad926cca7de919a86
Bruce Perry <bruce.perry@nrel.gov>
2025-10-08 15:15:29 -0600

    Warn for Manifold + Spray combo

--- Source/PeleLMeX_Setup.cpp
+++ Source/PeleLMeX_Setup.cpp
@@ -840,6 +840,10 @@ PeleLM::checkSetupParams()
       amrex::Abort("Soot models are not yet supported for Manifold EOS");
     }
 #endif
+#ifdef PELE_USE_SPRAY
+    amrex::Print()
+      << "WARNING: Spray models with Manifold EOS are experimental !!!!";
+#endif
 #ifdef PELE_USE_PLASMA
     amrex::Abort("Plasma models are not yet supported for Manifold EOS");
 #endif
