dcc755a8667c0e9edea5bc1599a3a366c2ff2e00
Bruce Perry <bruce.perry@nrel.gov>
2025-11-12 14:50:12 -0700

    add moving constant velocity reference frame option

--- Exec/RegTests/FlameSheet/pelelmex_prob.cpp
+++ Exec/RegTests/FlameSheet/pelelmex_prob.cpp
@@ -11,6 +11,9 @@ PeleLM::readProbParm()
   pp.query("pertmag", PeleLM::prob_parm->pertmag);
   pp.query("pertlength", PeleLM::prob_parm->pertlength);
   pp.query("ref_frame", PeleLM::prob_parm->ref_frame);
+  if (PeleLM::prob_parm->ref_frame == PmfReferenceFrame::fixed_velocity) {
+    pp.get("moving_frame_velocity", PeleLM::prob_parm->moving_frame_velocity);
+  }
 
   PeleLM::prob_parm->eosparm = PeleLM::eos_parms.device_parm();
   PeleLM::pmf_data.initialize();
