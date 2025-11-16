58eba4206e2055e809f3d170e4b91db4807866c7
SreejithNREL <Sreejith.NadakkalAppukuttan@nrel.gov>
2025-11-16 12:56:35 -0700

    removed redundant variables

--- Source/PeleLMeX_BPatch.cpp
+++ Source/PeleLMeX_BPatch.cpp
@@ -226,11 +226,9 @@ BPatch::BPatch(const std::string& patch_name, const amrex::Geometry& geom)
       }
 
       std::unordered_set<std::string> check_duplicate;
-      bool hasDuplicateSpecies = false;
       for (const auto& s : tokens) {
         if (!check_duplicate.insert(s)
-               .second) { // insert returns {iterator, success}
-          hasDuplicateSpecies = true;
+              .second) { // insert returns {iterator, success}
           std::string msg = "\nError! Duplicate species " + s +
                             " in group definition " + groupname_tmp +
                             "  in the patch " + patch_name;
