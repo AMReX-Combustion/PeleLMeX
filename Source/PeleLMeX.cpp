b73bbc5e705524301912123a5bfe48c141a59dce
snadakka <sreejith.nadakkalappukuttan@nrel.gov>
2026-01-29 17:55:42 -0500

    introduced reset functions for spray objects to resolve bugs on HIP

--- Source/PeleLMeX.cpp
+++ Source/PeleLMeX.cpp
@@ -32,8 +32,11 @@ PeleLM::~PeleLM()
   m_initial_ba.clear();
   m_regrid_ba.clear();
 #ifdef PELE_USE_SPRAY
-  SprayParticleContainer::SprayCleanUp();
-  SprayPC.reset();
+  //SprayParticleContainer::SprayCleanUp();
+  //SprayPC.reset();
+  SprayPC.reset(); 
+  GhostPC.reset();
+  VirtPC.reset();
 #endif
 #ifdef PELE_USE_SOOT
   cleanupSootModel();
