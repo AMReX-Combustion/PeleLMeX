36570bfe55cb6ae87efdc313f2a2dd09c14ae67f
Marc Day <Marcus.Day@sintef.no>
2026-06-09 20:14:11 +0200

    Fix PipeFlow example to use initdata_mapped

--- Exec/RegTests/PipeFlow/pelelmex_prob.cpp
+++ Exec/RegTests/PipeFlow/pelelmex_prob.cpp
@@ -14,22 +14,6 @@ PeleLM::readProbParm() // NOLINT(readability-make-member-function-const)
   pp.query("channelFullHeight", prob_parm->channelFullHeight);
   pp.query("problem_type", prob_parm->flowType);
   AMREX_ALWAYS_ASSERT(prob_parm->flowType == 1 || prob_parm->flowType == 2);
-
-  // If mesh mapping is active, pick up the ConstantMap scaling factors
-  // so the IC can be evaluated in physical coordinates.  Falls back to
-  // fac = 1 when mapping is off.
-  {
-    amrex::ParmParse ppcm("ConstantMap");
-    amrex::Vector<amrex::Real> fac(AMREX_SPACEDIM, 1.0);
-    ppcm.queryarr("scaling_factor", fac, 0, AMREX_SPACEDIM);
-    prob_parm->fac_x = fac[0];
-#if AMREX_SPACEDIM >= 2
-    prob_parm->fac_y = fac[1];
-#endif
-#if AMREX_SPACEDIM >= 3
-    prob_parm->fac_z = fac[2];
-#endif
-  }
 }
 
 void
