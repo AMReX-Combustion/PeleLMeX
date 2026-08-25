0836e8a783830142c4a8426f0f5b93ce55038d02
Marc Day <marcus.day@sintef.no>
2026-08-25 12:00:45 +0200

    The obligatory follow-up format fix

--- Source/Mesh/PeleLMeX_InteriorStretchMap.cpp
+++ Source/Mesh/PeleLMeX_InteriorStretchMap.cpp
@@ -141,9 +141,8 @@ InteriorStretchMap::InteriorStretchMap()
                    << " (s_c = " << m_s_c[d] << ")\n";
     if (ncell[d] > 0) {
       amrex::Print() << "                          cells "
-                     << static_cast<int>(std::lround(n_lo))
-                     << " below / " << static_cast<int>(std::lround(n_hi))
-                     << " above x_c"
+                     << static_cast<int>(std::lround(n_lo)) << " below / "
+                     << static_cast<int>(std::lround(n_hi)) << " above x_c"
                      << ", dx_min = " << m_Lxi[d] / (n * m_N[d])
                      << ", dx_max/dx_min = "
                      << std::fmax(
